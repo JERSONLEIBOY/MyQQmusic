f75eab8bf7f0fcc04ffd84467077b1cd834c64e0
Your Name <1017386624@qq.com>
2018-09-21 11:17:12 +0800

    解决轮播图插件的bug，仍有一些bug

--- src/pages/recommend/Recommend.vue
+++ src/pages/recommend/Recommend.vue
@@ -1,31 +1,38 @@
 <template>
   <div class="recommend">
+  	<!--轮播图部分-->
   	<div class="recommend-swiper">
-  	<swiper :options="swiperOption" ref="mySwiper">
-    	<!-- slides -->
-	    <swiper-slide v-for="item of sliders" :key="item.id"><img :src="item.picUrl"></swiper-slide>
-	    <!-- Optional controls -->
-	    <div class="swiper-pagination"  slot="pagination"></div>
-	    <div class="swiper-scrollbar"   slot="scrollbar"></div>
-	</swiper>
+	  	<swiper :options="swiperOption" ref="mySwiper">
+	    	<!-- 轮播图内容 -->
+		    <swiper-slide v-for="item of sliders" :key="item.id">
+		    	<img :src="item.picUrl">
+		    </swiper-slide>
+		    <!-- 轮播图小部件 -->
+		    <div class="swiper-pagination"  slot="pagination"></div>
+		</swiper>
 	</div>
-    这是推荐页面
   </div>
 </template>
 
 <script>
 import {getRecommend} from '@/api/recommend'	//引入api的后台数据
-import {ERR_OK} from '@/api/config'
+import {ERR_OK} from '@/api/config'	//引入自定义的公共变量
 
 export default {
   name: 'Recommend',
   data() {
       return {    	
         swiperOption: {
-          // some swiper options/callbacks
-          // 所有的参数同 swiper 官方 api 参数
-        	autoplay: 3000,
-			pagination: '.swiper-pagination',
+          //所有的参数，官方文档 注意语法都是对象
+        	autoplay: true,	//默认300可以设置，写成对象
+        	loop: true,	//bug：不生效，且自动播放，手动之后不会自动
+			pagination: {	//旧版本直接键值对，这里要对象
+				el:'.swiper-pagination'
+			},
+			/*navigation: {
+            	nextEl: '.swiper-button-next',
+           		prevEl: '.swiper-button-prev',
+           	},*/
 			observeParents:true,	//解决页面首次加载，滑动错误的问题
 			observer:true			//原因：首次加载的区域计算，使显示错误
         },
